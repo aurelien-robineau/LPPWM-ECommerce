eec9c08523bbf095147e539d00b689a539228612
Aurélien <robineau.aure@gmail.com>
2021-05-12 23:20:58 +0200

    Add shipping cost

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -18,13 +18,32 @@ class BasketController extends AbstractController
     {
         $user = $this->getUser();
     
-        $totalPrice = 0.00;
+        $productsPrice = 0.00;
         foreach ($user->getBasket() as $item) {
-            $totalPrice += $item->getProduct()->getPrice();
+            $productsPrice += $item->getProduct()->getPrice();
         }
 
+        $shippingPrice = self::getShippingPrice($productsPrice);
+
+        $totalPrice = $productsPrice + $shippingPrice;
+
         return $this->render('basket/index.html.twig', [
-            'totalPrice' => number_format($totalPrice, 2),
+            'shippingPrice' => $shippingPrice > 0 ? number_format($shippingPrice, 2) . ' €' : 'Offerts',
+            'totalPrice' => number_format($totalPrice, 2) . ' €'
         ]);
+    }
+
+    private static function getShippingPrice(int $orderPrice): float
+    {
+        if ($orderPrice >= 100)
+            return 0.0;
+        else if ($orderPrice >= 50)
+            return 4.99;
+        else if ($orderPrice >= 30)
+            return 9.99;
+        else if ($orderPrice >= 20)
+            return 14.99;
+        else
+            return 19.99;
     }
 }
